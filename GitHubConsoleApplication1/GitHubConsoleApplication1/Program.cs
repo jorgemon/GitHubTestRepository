90448df86ec4a89798540981463d7546cd712078
Jorge Montano <dennis.montano@jalasoft.com>
2013-02-27 22:20:32 -0400

    test from VS2012

--- GitHubConsoleApplication1/GitHubConsoleApplication1/Program.cs
+++ GitHubConsoleApplication1/GitHubConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace GitHubConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from GitHub");
-            Console.ReadKey();
+            
         }
     }
 }
